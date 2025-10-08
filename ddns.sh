10e7c51b66c0e9416e364a971d764866f5331a65
chinggirltube <93753629+chinggirltube@users.noreply.github.com>
2025-10-08 14:32:03 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -15,7 +15,7 @@ Tip="${YELLOW}[提示]${NC}"
 cop_info(){
 clear
 echo -e "${GREEN}#######################################################
-#      ${RED}Debian DDNS 一键脚本 ${GREEN}       #
+#      ${RED} DDNS 一键脚本 ${GREEN}       #
 #               作者: ${YELLOW}LAOWANG           ${GREEN}#
 #             https://github.com/chinggirltube                  ${GREEN}#
 #  ${YELLOW}优化: 缓存ZoneID, 重构更新逻辑, 加固文件权限${GREEN} #
